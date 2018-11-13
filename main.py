4c4274fb7a4171897424899ae53579aa1b3e8eaa
Rodolfo <rodolfoboffo@gmail.com>
2018-11-12 22:16:37 -0200

    Ensaio com classes de Sinais

--- main.py
+++ main.py
@@ -1,9 +1,77 @@
 import math
 import scipy.io.wavfile
 import numpy as np
+import fractions
 import matplotlib.pyplot as pyplot
 from subprocess import call
 import os
+
+class Signal(object):
+
+    def __init__(self, signal, sampleRate):
+        self.signal = signal
+        self.sampleRate = sampleRate
+
+    def getSampleRate(self):
+        return self.sampleRate
+
+    def get(self, index):
+        return self.signal[index]
+
+    def getRange(self, start=0, end=None):
+        i = start
+        j = end or len(self.signal)
+        s = []
+        while i < j:
+            return s.append(self.get(i))
+        return np.array(s)
+
+class PeriodicSignal(Signal):
+
+    def __init__(self, signal, sampleRate):
+        super(PeriodicSignal, self).__init__(signal, sampleRate)
+
+    def get(self, index):
+        return self.signal[index%len(self.signal)]
+
+class ConstantSignal(PeriodicSignal):
+
+    def __init__(self, const, sampleRate):
+        super(PeriodicSignal, self).__init__(np.array([const]), sampleRate)
+
+class OperationWithSignals(Signal):
+
+    def __init__(self, signal1, signal2, op):
+        if signal1.getSampleRate() != signal2.getSampleRate():
+            raise Exception("Sample rates doest'n match.")
+        self.signal1 = signal1
+        self.signal2 = signal2
+        self.op = op
+
+    def getSampleRate(self):
+        return self.signal1.getSampleRate()
+
+    def get(self, index):
+        return self.op(self.signal1.get(index), self.signal2.get(index))
+
+class Sum(OperationWithSignals):
+
+    def __init__(self, signal1, signal2):
+        op = lambda a, b: a + b
+        super(Sum, self).__init__(signal1, signal2, op)
+
+class Subtract(OperationWithSignals):
+
+    def __init__(self, signal1, signal2):
+        op = lambda a, b: a - b
+        super(Sum, self).__init__(signal1, signal2, op)
+
+class Multiply(OperationWithSignals):
+
+    def __init__(self, signal1, signal2):
+        op = lambda a, b: a * b
+        super(Sum, self).__init__(signal1, signal2, op)
+
 
 def getSineTable(sinusoidDetail):
     sin = []
